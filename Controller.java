a3d29f6aaea19fdd0a1c969f08a272e8b59afa53
FilGor <FilGor@github.com>
2021-04-11 15:59:45 +0200

    added dynamically loaded labels of UI

--- Controller.java
+++ Controller.java
@@ -6,6 +6,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.stream.Stream;
 
 public class Controller {
 
@@ -19,45 +26,44 @@ public class Controller {
     private TextField classNameField;
 
     @FXML
+    private Pane paneForClassInfo;
+
+    @FXML
     private TextArea bottomTextArea;
 
-    @FXML
-    private TextField firstParamField;
+    //pozycje startowe dynamicznie generowany labeli
+    private int positionYOfLabel = -30;
+    private int positionXOfLabel = 320;
+
+    Method classMethods[];
 
     @FXML
-    private TextField secondParamField;
+    void createObjectButtonClicked(ActionEvent event) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Class<?> cl = Class.forName(classNameField.getText()); // tworzenie obiektu reprezentujacego klase
+        Constructor<?> cons = cl.getDeclaredConstructor();//wyciagniecie ref obiektu reprezentujacego konstruktor
+        Object objectOfGivenClass = cons.newInstance();
+        classMethods = cl.getDeclaredMethods(); // wyciagniecie metod
+        Field classFields[] = cl.getDeclaredFields(); //wyciagniecie pól
 
-    @FXML
-    private TextField thirdParamField;
+        Stream.of(classFields) // wygenerowanie nazw pól
+                .map(e->e.getName())
+                .forEach(e-> {
+                    Label label = new Label(e);
+                    label.setTranslateX(positionXOfLabel);
+                    label.setTranslateY(positionYOfLabel+=40);
+                    paneForClassInfo.getChildren().add(label);
 
-    @FXML
-    private TextField fourthParamField;
-
-    @FXML
-    private TextArea fifthParamField;
-
-    @FXML
-    private Label firstParamLabel;
-
-    @FXML
-    private Label secondParamLabel;
-
-    @FXML
-    private Label thirdParamLabel;
-
-    @FXML
-    private Label fifthParamLabel;
-
-    @FXML
-    private Label fourthParamLabel;
-
-    @FXML
-    void createObjectButtonClicked(ActionEvent event) {
+                });
+        /* List <String> namesOfFields = Stream.of(classFields)
+                .map(e->e.getName())
+                .collect(Collectors.toList());
+        */
 
     }
 
     @FXML
     void saveChangesButtonClicked(ActionEvent event) {
+
 
     }
 
